6d976a7ccf83235049b138c918c820ccb5f45354
rusetochka <71005235+rusetochka@users.noreply.github.com>
2022-06-21 12:26:46 +0200

    uploading multiple files

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const methodOverride = require('method-override');
 const fs = require('fs');
-const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { Console } = require('console');
 //module for creating thumbnails
-const thumb = require('node-thumbnail').thumb;
+
 
 
 //connect to mongoose
@@ -29,34 +27,43 @@ router.get('/', function (req, res, next) {
     res.send('API is working properly');
 });
 
+//check type validity
+function typeValidation(type) {
+    const allowedFiles = ['image', 'pdf', 'text', 'msword', 'wordprocessingml', 'ms-excel', 'spreadsheetml'];
+    for (let i = 0; i < allowedFiles.length; i++) {
+        if (type.indexOf(allowedFiles[i]) !== -1) {
+            return true;
+        }
+    }
+    return false;
+}
 
 //single file upload
 router.post('/upload', async function (req, res) {
-      
+
     let sampleFile;
     let uploadPath;
-
     let mimetype;
-    const allowedFiles = ['image', 'pdf', 'text', 'msword', 'wordprocessingml', 'ms-excel', 'spreadsheetml'];
+    
 
+    //If no file attached, send 400 rsponse
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    // Retrieving the file(s) from html input field
     sampleFile = req.files.inputGroupFile02;
-    
-    
-    uploadPath = './public/uploads/' + sampleFile.name;
-    mimetype = req.files.inputGroupFile02.mimetype;
 
-    for (let i = 0; i < allowedFiles.length; i++) {
-        if (mimetype.indexOf(allowedFiles[i]) !== -1) {
+    //Uploading a single file
+    if (!sampleFile.length) {
+        uploadPath = './public/uploads/' + sampleFile.name;
+        mimetype = sampleFile.mimetype;
 
+        //if filetype is valid
+        if(typeValidation(mimetype)) {
                 //get the extention of a file
                 const arr = sampleFile.name.split('.');
                 const ext = arr[arr.length - 1];
-                
 
                 //preparing a new data for db
                 const newUser = {
@@ -66,37 +73,84 @@ router.post('/upload', async function (req, res) {
                     size: sampleFile.size,
                     downloaded: 0
                 }
+
                 // Place the file in a folder on server
-                sampleFile.mv(uploadPath, function(err) {
+                sampleFile.mv(uploadPath, function (err) {
                     if (err) {
                         return res.status(500).send(err);
-                    }      
+                    }
                 });
 
+                //preparing a thumbnail
+
+                //Place file info into database
                 await new Document(newUser)
                     .save()
                     .then(doc => {
                         if (req.hostname === 'localhost') {
-                        return res.redirect('http://localhost:3000')
+                            return res.redirect('http://localhost:3000')
                         } else {
                             return res.status(204).redirect(`${req.protocol}://${req.hostname}/`);
                         }
                     });
-
-
-  
         }
-        
-    }
-    return res.status(403).send('This type of file is not allowed.');
+        //if filetype is not valid
+        return res.status(403).send('This type of file is not allowed.');
+    } else {
 
+        //uploading multiple files
+        for(let i = 0; i < sampleFile.length; i++) {
+            uploadPath = './public/uploads/' + sampleFile[i].name;
+            mimetype = sampleFile[i].mimetype;
+
+            //if type is valid
+            if(typeValidation(mimetype)) {
+              //get the extention of a file
+              const arr = sampleFile[i].name.split('.');
+              const ext = arr[arr.length - 1];
+
+              //preparing a new data for db
+              const newUser = {
+                  filename: sampleFile[i].name,
+                  file: sampleFile[i],
+                  extention: ext,
+                  size: sampleFile[i].size,
+                  downloaded: 0
+              }
+
+              // Place the file in a folder on server
+              sampleFile[i].mv(uploadPath, function (err) {
+                  if (err) {
+                      return res.status(500).send(err);
+                  }
+              });
+
+              //preparing a thumbnail
+
+              //Place file info into database
+              await new Document(newUser)
+                  .save()
+                  .then(doc => {
+                      if (req.hostname === 'localhost') {
+                          return res.redirect('http://localhost:3000')
+                      } else {
+                          return res.status(204).redirect(`${req.protocol}://${req.hostname}/`);
+                      }
+                  });  
+            } else {
+                //if filetype is not valid
+                return res.status(403).send('This type of file is not allowed.');
+            }
+            
+        }
+    }
 });
 
 //load all uploaded files
 router.get('/uploads', async (req, res) => {
 
     Document.find({})
-        .sort({dateOfUpload: 'desc'})
+        .sort({ dateOfUpload: 'desc' })
         .then(docs => {
             res.status(200).send({
                 documents: docs
@@ -109,17 +163,17 @@ router.get('/uploads', async (req, res) => {
 router.delete('/uploads/:id', (req, res) => {
     let filename;
     //delete file from server
-    Document.findOne({"_id": req.params.id})
-    .then(doc => {
-        filename = doc.filename;
-        fs.unlink(`./public/uploads/${filename}`, (err) => {
-            if (err) throw err;
-            console.log(`${filename} was deleted`);
-          })
-    } );
-    
+    Document.findOne({ "_id": req.params.id })
+        .then(doc => {
+            filename = doc.filename;
+            fs.unlink(`./public/uploads/${filename}`, (err) => {
+                if (err) throw err;
+                console.log(`${filename} was deleted`);
+            })
+        });
+
     //delete file from the database
-    Document.deleteOne({"_id": req.params.id})
+    Document.deleteOne({ "_id": req.params.id })
         .then(() => {
             return res.status(200).redirect('http://localhost:3000');
         }, () => {
@@ -130,35 +184,35 @@ router.delete('/uploads/:id', (req, res) => {
 //Download a document
 router.get('/uploads/:id', (req, res) => {
     let filename;
-    Document.findOne({"_id": req.params.id})
-        .then(doc =>  {
+    Document.findOne({ "_id": req.params.id })
+        .then(doc => {
             filename = doc.filename;
             res.download(`./public/uploads/${filename}`);
             doc.downloaded += 1;
             doc.save();
         });
-    
+
 });
 
 //Download a document from the shared link
 router.get('/uploads/:id/:timestamp', (req, res) => {
     let timestamp = req.params.timestamp;
     let currentDate = new Date().getTime();
-    const diff = (currentDate - timestamp)/60000;
-    if(diff > 5) {
+    const diff = (currentDate - timestamp) / 60000;
+    if (diff > 5) {
         res.status(500).send('Your link is not available anymore.');
     } else {
         let filename;
-    Document.findOne({"_id": req.params.id})
-        .then(doc =>  {
-            filename = doc.filename;
-            res.download(`./public/uploads/${filename}`);
-            doc.downloaded += 1;
-            doc.save();
-        });
+        Document.findOne({ "_id": req.params.id })
+            .then(doc => {
+                filename = doc.filename;
+                res.download(`./public/uploads/${filename}`);
+                doc.downloaded += 1;
+                doc.save();
+            });
     }
 
-    
+
 })
 
 
